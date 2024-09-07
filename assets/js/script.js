b5a77a0f511ed9dfb86c4d91e4564f55270d036d
Tori-coder <114353884+Tori-coder@users.noreply.github.com>
2024-09-07 13:52:27 +0100

    Add displayNextIdiom function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,14 +20,17 @@ const idiomsArray = [
 ]
 
 //define variables
-let currentQuestionIndex
-let score = 0;
+let currentQuestionIndex;
+let currentIdiomIndex;
+let score;
 
 //define elements
 const idiomElement = document.getElementById("idiom");
 const optionButtonA = document.getElementById("option-btn-a");
 const optionButtonB = document.getElementById("option-btn-b");
 const optionButtonC = document.getElementById("option-btn-c");
+const rightWrongElement = document.getElementById("right-or-wrong")
+const scoreElement = document.getElementById("score");
 
 //define functions
 function displayStartScreen() {
@@ -35,48 +38,57 @@ function displayStartScreen() {
 }
 
 function optionAClickHandler() {
-    checkAnswer(idiomsArray[currentQuestionIndex].meanings[0]);
+    checkAnswer(idiomsArray[currentIdiomIndex].meanings[0]);
 }
 
 function optionBClickHandler() {
-    checkAnswer(idiomsArray[currentQuestionIndex].meanings[1]);
+    checkAnswer(idiomsArray[currentIdiomIndex].meanings[1]);
 }
 
 function optionCClickHandler() {
-    checkAnswer(idiomsArray[currentQuestionIndex].meanings[2]);
+    checkAnswer(idiomsArray[currentIdiomIndex].meanings[2]);
 }
 
 function runQuiz() {
     currentQuestionIndex = 0;
+    currentIdiomIndex = 0;
+    score = 0;
     displayCurrentIdiom();
     // Add event listeners to option buttons
     document.getElementById("option-btn-a").addEventListener("click", optionAClickHandler);
     document.getElementById("option-btn-b").addEventListener("click", optionBClickHandler);
     document.getElementById("option-btn-c").addEventListener("click", optionCClickHandler);
-    // Add event listener to next button
-    document.getElementById("submit-btn").addEventListener("click", displayNextIdiom);
-    displayNextIdiom();
   }
 
 function displayCurrentIdiom() {
     //choose and display a random idiom from idiomsArray
-    let currentIdiomIndex = Math.floor(Math.random() * idiomsArray.length);
-    idiomElement.innerText = idiomsArray[currentQuestionIndex].idiom;
+    currentIdiomIndex = Math.floor(Math.random() * idiomsArray.length);
+    console.log(currentIdiomIndex);
+    idiomElement.innerText = idiomsArray[currentIdiomIndex].idiom;
     //display meanings options
-    optionButtonA.innerText = idiomsArray[currentQuestionIndex].meanings[0];
-    optionButtonB.innerText = idiomsArray[currentQuestionIndex].meanings[1];
-    optionButtonC.innerText = idiomsArray[currentQuestionIndex].meanings[2];
+    optionButtonA.innerText = idiomsArray[currentIdiomIndex].meanings[0];
+    optionButtonB.innerText = idiomsArray[currentIdiomIndex].meanings[1];
+    optionButtonC.innerText = idiomsArray[currentIdiomIndex].meanings[2];
 }
 
 function displayNextIdiom() {
-
+    // Clear the inner HTML content of "right-or-wrong" element
+    document.getElementById("right-or-wrong").innerText = "Select a meaning";
+    //increase the question index
+    currentQuestionIndex++;
+    //check to see if 5 questions have been asked, and if so, run endOfQuiz
+    if (currentQuestionIndex===5) {
+    endOfQuiz();
+    } else {
+    //generate a new random number to choose the current idiom from the array
+    currentIdiomIndex = Math.floor(Math.random() * idiomsArray.length);
+    //display the new idiom
+    displayCurrentIdiom();
+    }
 }
 
 function checkAnswer(selectedAnswer) {
-    const rightWrongElement = document.getElementById("right-or-wrong")
-    const scoreElement = document.getElementById("score");
-
-    if (idiomsArray[currentQuestionIndex].answer === selectedAnswer) {
+    if (idiomsArray[currentIdiomIndex].answer === selectedAnswer) {
       rightWrongElement.innerText = "You got it right";
       score++;
       scoreElement.innerText = `Your score is: ${score} out of 10`;
@@ -84,6 +96,8 @@ function checkAnswer(selectedAnswer) {
         rightWrongElement.innerText = "You got it wrong";
       scoreElement.innerText = `Your score is: ${score} out of 10`;
     }
+    // Add event listener to next button
+    document.getElementById("next-btn").addEventListener("click", displayNextIdiom);
 }
 
 function startNewQuiz() {
